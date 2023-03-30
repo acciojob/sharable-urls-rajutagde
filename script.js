2e8d4be5655686cfb9845cf61ecd105f01387734
Rajkirantagde <121598976+rajutagde@users.noreply.github.com>
2023-03-30 11:52:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 // your code here
+function updateURL() {
+			// Get input values
+			var name = document.getElementById("name").value;
+			var year = document.getElementById("year").value;
+
+			// Construct URL query string
+			var queryString = "?name=" + encodeURIComponent(name) + "&year=" + encodeURIComponent(year);
+
+			// Update URL in h3 element
+			document.getElementById("url").textContent = "https://localhost:8080/" + queryString;
+		}
+	</script>
